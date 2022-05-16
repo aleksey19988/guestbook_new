aaa9fe557333910b1a8205f44550837224668219
Aleksey <59168106+aleksey19988@users.noreply.github.com>
2022-05-16 17:19:01 +0300

    Добавил пагинацию
    1. Разделил получение контента на два метода:
    1.1. Получить все контакты (для последующего получения кол-ва страниц и манипуляций со всеми контактами одновременно)
    1.2. Получить контакты с пагинацией (для возможности перемещаться по страницам)

--- Content.php
+++ Content.php
@@ -3,25 +3,25 @@ require_once 'DBconnect.php';
 
 class Content
 {
-    public function showComments()
+    private int $commentsCountOnPage = 5;
+
+    public function getAllComments(): array
     {
         $connection = DBconnect::connectToDB();
-        $query = 'SELECT * FROM comments';
-        $comments = $connection->query($query)->fetchAll();
-        $counter = 1;
+        $query = "SELECT * FROM comments";
 
-        foreach ($comments as $comment) {
-            $row = "<td>$counter</td>";
-            foreach ($comment as $key => $value) {
-                if ($key === 'id') {
-                    continue;
-                } elseif (gettype($key) === 'string') {
-                    $row .= "<td>$value</td>";
-                }
-            }
-            print_r("<tr>{$row}</tr>");
-            $counter++;
-        }
+        return $connection->query($query)->fetchAll();
+    }
+
+    public function getCommentsWithPaging($page = 1): array
+    {
+        $page = $_GET['page'] ?? $page;
+        $offset = ($page > 1) ? $this->commentsCountOnPage * ($page - 1) : 0;
+
+        $connection = DBconnect::connectToDB();
+        $query = "SELECT * FROM comments LIMIT {$offset}, {$this->commentsCountOnPage}";
+
+        return $connection->query($query)->fetchAll();
     }
 
     public function showTableHead()
@@ -39,5 +39,35 @@ class Content
         }
 
         print_r("<thead>{$thead}</thead>");
+    }
+
+    public function showComments()
+    {
+        $comments = $this->getCommentsWithPaging();
+//        $counter = ($_GET['page'] * $this->commentsCountOnPage) ?? 1;
+        $counter = ($_GET['page'] == 1 || $_GET['page'] === null) ? 1 : (($_GET['page'] - 1) * $this->commentsCountOnPage + 1);
+        foreach ($comments as $comment) {
+            $row = "<td>$counter</td>";
+            foreach ($comment as $key => $value) {
+                if ($key === 'id') {
+                    continue;
+                } elseif (gettype($key) === 'string') {
+                    $row .= "<td>$value</td>";
+                }
+            }
+            print_r("<tr>{$row}</tr>");
+            $counter++;
+        }
+    }
+
+    public function getPagesCount(): int
+    {
+        $pagesCount = ceil(count($this->getAllComments()) / $this->commentsCountOnPage);
+
+        for ($i = 1; $i <= $pagesCount; $i++) {
+            print_r("<a href=?page={$i}><button class='page-button'>{$i}</button></a>");
+        }
+
+        return $pagesCount;
     }
 }
--- index.php
+++ index.php
@@ -6,61 +6,64 @@ $content = new Content();
 <!doctype html>
 <html lang="en">
     <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Guestbook</title>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-      <link rel="stylesheet" href="style/style.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Guestbook</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <link rel="stylesheet" href="style/style.css">
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Guestbook</a>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav authorization-buttons">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Sign In</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Sign Up</a>
-                        </li>
-                    </ul>
-                </div>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Guestbook</a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav authorization-buttons">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Sign In</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Sign Up</a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-        <div class="container form-content">
-            <form action="saveCommentToDB.php" method="POST">
-                <div class="mb-3">
-                    <label for="inputUserName" class="form-label">User Name<span class="required-input-tag">*</span></label>
-                    <input type="text" class="form-control" id="inputUserName" name="userName" required>
-                </div>
-                <div class="mb-3">
-                    <label for="inputEmail" class="form-label">Email address<span class="required-input-tag">*</span></label>
-                    <input type="email" class="form-control" id="inputEmail" name="email" aria-describedby="emailHelp" required>
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div class="mb-3">
-                    <label for="inputHomepage" class="form-label">Homepage</label>
-                    <input type="url" placeholder="https://example.com" pattern="https://.*" class="form-control" id="inputHomepage" name="homepage">
-                </div>
-                <div class="mb-3">
-                    <label for="inputText" class="form-label">Text<span class="required-input-tag">*</span></label>
-                    <input type="text" class="form-control" id="inputText" name="text" required>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
         </div>
-        <div class="container table-content">
-            <table class="table table-hover">
-                <?php $content->showTableHead(); ?>
-                <?php $content->showComments(); ?>
-            </table>
-        </div>
+    </nav>
+    <div class="container form-content">
+        <form action="saveCommentToDB.php" method="POST">
+            <div class="mb-3">
+                <label for="inputUserName" class="form-label">User Name<span class="required-input-tag">*</span></label>
+                <input type="text" class="form-control" id="inputUserName" name="userName" required>
+            </div>
+            <div class="mb-3">
+                <label for="inputEmail" class="form-label">Email address<span class="required-input-tag">*</span></label>
+                <input type="email" class="form-control" id="inputEmail" name="email" aria-describedby="emailHelp" required>
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="inputHomepage" class="form-label">Homepage</label>
+                <input type="url" placeholder="https://example.com" pattern="https://.*" class="form-control" id="inputHomepage" name="homepage">
+            </div>
+            <div class="mb-3">
+                <label for="inputText" class="form-label">Text<span class="required-input-tag">*</span></label>
+                <input type="text" class="form-control" id="inputText" name="text" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    <div class="container table-content">
+        <table class="table table-hover">
+            <?php $content->showTableHead(); ?>
+            <?php $content->showComments(); ?>
+        </table>
+    </div>
+    <div class="container page-list" align="center">
+        <?php $content->getPagesCount(); ?>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     </body>
 </html>
